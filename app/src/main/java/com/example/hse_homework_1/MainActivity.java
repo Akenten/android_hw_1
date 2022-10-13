37bd5df2b62da46dcda0fbec86174960f450f8aa
Akenten <ars.alex19@yandex.ru>
2022-10-13 15:05:22 +0300

    Cleaned the code

--- app/src/main/java/com/example/hse_homework_1/MainActivity.java
+++ app/src/main/java/com/example/hse_homework_1/MainActivity.java
@@ -1,22 +1,18 @@
 package com.example.hse_homework_1;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.SearchManager;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button activitybutton;
-    Button mapButton;
-    Button counterButton;
+    Button searchButton;
     TextView recivedText;
 
     @Override
@@ -25,40 +21,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         activitybutton = findViewById(R.id.activity_button);
-        mapButton = findViewById(R.id.mapButton);
-
+        searchButton = findViewById(R.id.SearchButton);
         recivedText = findViewById(R.id.showText);
 
-        String c = "";
-
-
-        
         
         Intent received = getIntent();
-       // c = received.getStringExtra("counter");
-        Toast.makeText(MainActivity.this, c, Toast.LENGTH_LONG).show();
         String str = received.getStringExtra("key");
         recivedText.setText(str);
+
         activitybutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent myintent = new Intent(MainActivity.this, MainActivity2.class);
                 startActivity(myintent);
-
             }
         });
-        mapButton.setOnClickListener(new View.OnClickListener(){
+
+        searchButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-                intent.putExtra(SearchManager.QUERY, str); // query contains search string
+                intent.putExtra(SearchManager.QUERY, str);
                 startActivity(intent);
                 }
-
         });
-
-
-
 
 
     }
